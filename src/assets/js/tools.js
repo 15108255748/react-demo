59323e1715a73b4734638227a58cc7c1917a22c3
pdc <390509581@qq.com>
2020-03-31 15:29:55 +0800

    首次提交

---
+++ src/assets/js/tools.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion:  通用工具类 挂载实例对象
+ * @version: 1.0
+ * @Author: pdc
+ * @Date: 2020-03-31 15:21:21
+ * @LastEditors: pdc
+ * @LastEditTime: 2020-03-31 15:22:46
+ */
---
+++ src/route/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion: 路由配置文件
+ * @version: 1.0
+ * @Author: pdc
+ * @Date: 2020-03-31 15:22:54
+ * @LastEditors: pdc
+ * @LastEditTime: 2020-03-31 15:23:03
+ */
