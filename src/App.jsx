6995dff6d6c3402d20821396c0a0a6cce28174c2
pdc <390509581@qq.com>
2020-03-31 16:43:10 +0800

    添加axios模块,请求后端地址接口

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,23 @@
  * @Author: pdc
  * @Date: 2020-03-31 13:57:50
  * @LastEditors: pdc
- * @LastEditTime: 2020-03-31 16:20:58
+ * @LastEditTime: 2020-03-31 16:41:50
  */
 import React, { Component } from 'react';
 import '@/assets/css/global.css';
+import { Button } from 'antd';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.wrapper = React.createRef()
+  }
+
   render() {
     return <div className="App" >
-
+      <Button ref={this.wrapper} type="primary">测试</Button>
     </div>
   };
 }
